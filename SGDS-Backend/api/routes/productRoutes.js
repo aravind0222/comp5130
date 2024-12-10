819379468a746c6724b326c1da7719ea04eaf11f
aravind0222 <aravindy2222@gmail.com>
2024-12-10 15:07:29 -0500

    Delete SGDS-Backend directory

--- SGDS-Backend/api/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/productController");
-/* @swagger
- */
-router.get("/all", productController.getProducts);
-
-module.exports = router;
